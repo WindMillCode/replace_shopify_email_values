ddb3c8a216372e605006b8d88ff7ca67ac52b5cd
windmillcode0 <michaelodumosu57@gmail.com>
2024-07-09 19:26:38 -0400

    [UPDATE] additional work

--- extension/content.js
+++ extension/content.js
@@ -7,9 +7,12 @@ function bulkConvertAllEmails({
   storeURL,
   storeURLText
 }) {
-
+  console.log("ready to convert")
+  editBtn = document.querySelector("#settings-body > div > div.Polaris-Box > div.Polaris-Page-Header--mediumTitle > div > div.Polaris-Page-Header__RightAlign > div.Polaris-Page-Header__PrimaryActionWrapper > div > button")
+  console.log(editBtn)
 }
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request)
   if(request.msg.type === "StartJob"){
     bulkConvertAllEmails(request.msg.value)
   }
--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,51 @@
-document.addEventListener('DOMContentLoaded',async () => {
-  
+
+
+async function sendMsgToWebsite(msg) {
+
+  chrome.windows.getCurrent(w => {
+    chrome.tabs.query({active: true, windowId: w.id}, async(tabs) => {
+      const tabId = tabs[0].id;
+      const response = await chrome.tabs.sendMessage(tabId, {msg});
+
+    });
+  });
+
+}
+
+startJob = ()=>{
+  const mediaOptions = [
+    "removeSources",
+    "showContactEmail"
+  ];
+  value = {}
+  mediaOptions.forEach(option => {
+    const checkbox = document.getElementById(option);
+
+    value[option] = checkbox.checked;
+
+  });
+  const inputs = [
+    "storeURL",
+    "storeURLText"
+  ];
+  inputs.forEach(option => {
+    const myInput = document.getElementById(option);
+
+    if (myInput) {
+      value[option] =myInput.value
+    }
+  });
+  sendMsgToWebsite({
+    type:"StartJob",
+    value
+  })
+}
+
+
+window.addEventListener('DOMContentLoaded',async () => {
+  let btn = document.querySelector("#startJob")
+  btn.addEventListener("click",startJob)
+  let removeSources = document.getElementById("removeSources");
+  removeSources.checked = true;
+
 })
